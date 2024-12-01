89696140cbca00832c21eb0dd22e77988323d151
Denys-Tym <133146642+Denys-Tym@users.noreply.github.com>
2024-12-02 00:23:46 +0100

    Commit_3_JS1

--- JS/task-3.js
+++ JS/task-3.js
@@ -1,9 +1,9 @@
 function getElementWidth(content, padding, border) {
-  return `${content + padding * 2 + border * 2}`;
-  getElementWidth(50, 8, 4);
-  getElementWidth(60, 12, 8.5);
-  getElementWidth(200, 0, 0);
+  return Number.parseInt(content) + Number.parseInt(padding) * 2 + Number.parseFloat(border) * 2;
+  getElementWidth("50px", "8px", "4px");
+  getElementWidth("60px", "12px", "8.5px");
+  getElementWidth("200px", "0px", "0px");
 }
-console.log(getElementWidth(50, 8, 4)); // 74
-console.log(getElementWidth(60, 12, 8.5)); // 101
-console.log(getElementWidth(200, 0, 0)); // 200
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
