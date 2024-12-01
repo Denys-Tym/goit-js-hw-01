4af64b79558daf6dc241d2f885dfddff8299dda9
Denys-Tym <133146642+Denys-Tym@users.noreply.github.com>
2024-12-01 23:25:21 +0100

    Commit-JS_1.2

---
+++ JS/task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+  return `${content + padding * 2 + border * 2}`;
+  getElementWidth(50, 8, 4);
+  getElementWidth(60, 12, 8.5);
+  getElementWidth(200, 0, 0);
+}
+console.log(getElementWidth(50, 8, 4)); // 74
+console.log(getElementWidth(60, 12, 8.5)); // 101
+console.log(getElementWidth(200, 0, 0)); // 200
